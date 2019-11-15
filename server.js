199cadf82e0abad1780a7301cecb094ed76342ea
Glitch (sudsy-quicksand) <none>
2019-11-15 07:50:36 +0000

    🛫🍀 Updated with Glitch

--- server.js
+++ server.js
@@ -1,23 +1,32 @@
-'use strict';
+"use strict";
 
-var express = require('express');
-var cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 // require and use "multer"...
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 
-var app = express();
+const app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-     res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+     res.sendFile(process.cwd() + "/views/index.html");
   });
 
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
+app.post("/api/fileanaly[sz]e", upload.single("upfile"), (req, res) => {
+  const { file } = req;
+
+  res.json({
+    name: file.originalname,
+    type: file.mimetype,
+    size: file.size
+  });
+
 });
 
 app.listen(process.env.PORT || 3000, function () {
-  console.log('Node.js listening ...');
+  console.log("Node.js listening ...");
 });
